90aaf051219c139dd86a047bfdf3ca825d0eb879
Matt Shaw <Shaw-M18@ulster.ac.uk>
2024-12-06 15:04:06 +0000

    fixed the migration foreign id issue

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -9,5 +9,9 @@ class Category extends Model
 {
     use HasFactory;
 
-    protected $fillable = ['name', '']
+    protected $fillable = ['name'];
+
+    public function products() {
+        return $this->hasMany(Product::class);
+    }
 }
