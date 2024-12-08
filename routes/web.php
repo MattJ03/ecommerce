29d811d67e02886e22579675fc98cd4c0029e12b
Matt Shaw <Shaw-M18@ulster.ac.uk>
2024-12-08 23:45:07 +0000

    routes for Product

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\CategoryController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,4 +14,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-
+Route::get('/products', [ProductController::class, 'index'])->name('products.index');
+Route::post('/product/create', [ProductController::class, 'create'])->name('products.create');
+Route::get('/products/{id}', [ProductController::class, 'show'])->name('products.show');
+Route::get('/products/{id}/edit', [ProductController::class, 'edit'])->name('products.edit');
+Route::put('/products/{id}', [ProductController::class, 'update'])->name('products.update');
+Route::delete('/products/{id}', [ProductController::class, 'destroy'])->name('products.destroy');
