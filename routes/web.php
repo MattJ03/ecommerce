59ef883f06ddab98e23f47b6e9311bcea219922f
Matt Shaw <Shaw-M18@ulster.ac.uk>
2024-12-09 00:01:55 +0000

    added categories functionaltiy to route

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,9 @@ Route::get('/products/{id}', [ProductController::class, 'show'])->name('products
 Route::get('/products/{id}/edit', [ProductController::class, 'edit'])->name('products.edit');
 Route::put('/products/{id}', [ProductController::class, 'update'])->name('products.update');
 Route::delete('/products/{id}', [ProductController::class, 'destroy'])->name('products.destroy');
+
+Route::get('/categories', [CategoryController::class, 'index'])->name('categories.index');
+Route::post('/category/create', [CategoryController::class, 'create'])->name('categories.create');
+Route::get('/categories/{id}/edit', [CategoryController::class, 'edit'])->name('categories.edit');
+Route::put('/categories/{id}', [CategoryController::class, 'update'])->name('categories.update');
+Route::delete('/categories/{id}', [CategoryController::class, 'destroy'])->name('categories.destroy');
